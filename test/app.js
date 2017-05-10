9b4ddd2cf232734ecaddff4de0dc43b08a1f9cfc
vikas91 <chandrashekar.v@husky.neu.edu>
2017-05-10 15:53:31 -0400

    added code for heroku
    added code for heroku

--- test/app.js
+++ test/app.js
@@ -4,14 +4,13 @@ module.exports = function(app)
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
-    var connectionString = 'mongodb://127.0.0.1:27017/test';
+    var connectionString = 'mongodb://127.0.0.1:27017/test'; //local
 
-    if(process.env.MLAB_USERNAME) {
-        connectionString = process.env.MLAB_USERNAME + ":" +
-            process.env.MLAB_PASSWORD + "@" +
-            process.env.MLAB_HOST + ':' +
-            process.env.MLAB_PORT + '/' +
-            process.env.MLAB_APP_NAME;
+    if(process.env.MONGODB_USERNAME) {
+        var username = process.env.MONGODB_USERNAME;
+	var password = process.env.MONGODB_PASSWORD;
+	connectionString = 'mongodb://' + username + ':' + password;
+	connectionString += '@ds017248.mlab.com:17248/heroku_lbh4f8m5';
     }
 
     var mongoose = require("mongoose");
