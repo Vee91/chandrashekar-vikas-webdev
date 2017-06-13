01fa0fd281710c26354451a46cac608de24de085
vikas91 <chandrashekar.v@husky.neu.edu>
2017-06-13 03:55:21 -0400

    db corrected 2
    db corrected 2

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -1,6 +1,13 @@
 var mongoose = require("mongoose");
 var connectionString = 'mongodb://localhost/webdev';
 
+if(process.env.MONGODB_USERNAME) {
+    var username = process.env.MONGODB_USERNAME;
+    var password = process.env.MONGODB_PASSWORD;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds017248.mlab.com:17248/heroku_lbh4f8m5';
+}
+
 mongoose.connect(connectionString);
 var userModel = require("./user/user.model.server")();
 var websiteModel = require("./website/website.model.server")(userModel);
